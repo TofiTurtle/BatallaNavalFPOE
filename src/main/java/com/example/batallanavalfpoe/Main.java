b21dac3e3caaac3e38cc15e72db06d36417db20e
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2025-05-12 20:55:46 -0500

    commit prueba conexion github

--- src/main/java/com/example/batallanavalfpoe/Main.java
+++ src/main/java/com/example/batallanavalfpoe/Main.java
@@ -3,5 +3,6 @@ package com.example.batallanavalfpoe;
 public class Main {
     public static void main(String[] args) {
         System.out.println("perrita");
+        System.out.println("prueba conexion github");
     }
 }
